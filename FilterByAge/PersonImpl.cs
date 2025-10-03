707a64404cac142406522042d7d1928376e8fb43
Zlatko33Kamenov <138592118+Zlatko33Kamenov@users.noreply.github.com>
2025-10-03 16:58:20 +0300

    FilterByAge-V1

---
+++ FilterByAge/PersonImpl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilterByAge
+{
+    public static class PersonImpl
+    {
+        public static Predicate<Person> FilterByAge(string condition, int age)
+        {
+            return condition switch
+            {
+                "younger" => p => p.Age < age,
+                "older" => p => p.Age >= age,
+                _ => null!
+            };
+        }
+    }
+}
---
+++ FilterByAge/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace FilterByAge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPeople = int.Parse(Console.ReadLine());
+
+            List<Person> people = new();
+            for (int i = 0; i < numberOfPeople; i++)
+            {
+                var data = Console.ReadLine()
+                           .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                people.Add(new Person(data[0], int.Parse(data[0])));
+            }
+            var condition = Console.ReadLine();
+            var years = int.Parse(Console.ReadLine());
+
+            Predicate<Person> filterByAge = PersonHelper.FilterByAge(condition, years);
+            List<Person> filteredByAge = people.FindAll(filterByAge);
+
+            var format = Console.ReadLine();
+            Action<Person> printBy = PersonHelper.PrintPerson(format);
+
+            filteredByAge.ForEach(PersonHelper.PrintPerson);
+        }
+    }
+}
